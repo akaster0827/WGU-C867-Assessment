2cc2a989debe54c83d45baeee19a188ef054bdf3
akaster0827 <akaster0827@gmail.com>
2020-02-19 00:02:45 -0600

    Add files via upload

--- roster.cpp
+++ roster.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
+#include "securityStudent.h"
+#include "student.h"
+#include "roster.h"
+#include "softwareStudent.h"
+#include "networkStudent.h"
+
+
+int main() {
+	cout << "SCRIPTING AND PROGRAMMING APPLICATIONS - C867" << endl;
+	cout << "Programming Language: C++ - Written by Alex Kaster, ID: #000783857\n" << endl;
+
+	Roster classRoster;
+	Degree degree;
+
+	string output, col[DATA_COLUMNS];
+
 
